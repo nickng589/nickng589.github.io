2c228dd57102928eefca3e51dc8ed8085ea254ca
Nicholas Ng <nickng589@berkeley.edu>
2020-01-01 03:49:11 -0800

    js

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+var i = 0;
+var txt = 'Lorem ipsum dummy text blabla.';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+function fadeOut() {
+    var fadeTarget = document.getElementById("demo");
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 50);
+}
+
+window.onload = typeWriter;
